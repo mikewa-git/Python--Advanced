fef87c2bbad44e9e5157178e52b6ba17ee9c4762
Mike <mwang822@gmail.com>
2023-05-14 16:59:01 -0700

    INIT 03_advanced

---
+++ 03_advanced/assignment_advanced.py
@@ -0,0 +1,74 @@
+# ADVANCED **************************************************************************
+# content = assignment (Python Advanced)
+#
+# deliver = Upload the files to your git repository
+#           and share it in the assignment submission form.
+#
+# date    = 2022-08-07
+# email   = contact@alexanderrichtertd.com
+#*******************************************************************************
+"""
+
+Advanced Python is about a higher and more structural level of Python.
+Learning new functions, constructs and a better way of collaboration.
+
+
+
+#*******************************************************************************
+# 01. CUBE CLASS
+#*******************************************************************************
+Create a cube class in cube.py
+
+
+
+
+
+
+
+#*******************************************************************************
+# 02. DECORATOR
+#*******************************************************************************
+   a) Create a decorator in decorator.py
+
+   b) Include a decorator into your own application.
+
+
+
+
+
+#*******************************************************************************
+# 03. CLASSES & PARENTING
+#*******************************************************************************
+
+   a) Copy your latest application into "03_advanced"
+
+   b) Commit and push the "old" application version to the "develop" branch
+
+   c) Update one of your modules using classes.
+      Make sure to use one of the benefits of classes (else it's pointless).
+      Write a comment in the docString of the class why a class would improve this module.
+
+   d) OPTIONAL: Update one of your modules using class parenting.
+      Write a comment in the docString of the class why parenting would improve this module.
+
+      NOTE: c) & d) can be combined.
+
+
+
+
+
+
+
+#*******************************************************************************
+# 04. CONFIGURATION FILES
+#*******************************************************************************
+
+   a) Update one of your modules using configuration or data files.
+
+   b) Write a comment in the call of the configuration file
+      why a configuration or data file would improve this module.
+
+   c) Commit and push the updated version to "main/master".
+
+
+"""
